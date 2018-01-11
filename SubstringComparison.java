f4f08f868019f219e0157e3ad70f3aa70b930fd0
Eugene Voznesensky <eugene.voznesensky@itsoninc.com>
2018-01-10 19:05:38 -0800

    SubstringComparison.java

---
+++ SubstringComparison.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        smallest = largest = s.substring(0, k);
+        for (int i = 1; i < s.length() - k + 1; i++) {
+            String current = s.substring(i, i + k);
+            //System.out.println("current: " + current);
+            if (s.substring(i, i + k).compareTo(smallest) <= 0) {
+                smallest = s.substring(i, i + k);
+            }
+            //System.out.println("smallest: " + smallest);
+            
+            if (s.substring(i, i + k).compareTo(largest) > 0) {
+                largest = s.substring(i, i + k);
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+
